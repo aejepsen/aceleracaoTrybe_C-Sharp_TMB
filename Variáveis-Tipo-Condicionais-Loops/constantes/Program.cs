f8cd27ccf1a56fdf4b12a3e125e3c9ff412f7526
allan <aejepsen@yahoo.com.br>
2023-02-17 18:51:30 -0300

    ok

---
+++ Variáveis-Tipo-Condicionais-Loops/constantes/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        //esta variável será compilada como string
+        var school = "Trybe";
+
+        //esta variável será compilada como int
+        var semester = 1;
+
+        //esta variável será compilada como um tipo anônimo
+        var student = new { name = "Jorge", age = 30 };
+
+        Console.WriteLine("Olá! Me chamo " + student.name + " " +
+                          "sou um estudante da escola " + school + " " +
+                          "estou no " + semester + "º semestre " +
+                          "e tenho " + student.age + " anos"); 
+    }
+}
--- Variáveis-Tipo-Condicionais-Loops/struct/Program.cs
+++ Variáveis-Tipo-Condicionais-Loops/struct/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿struct Schedule
+{
+    public int Day;
+    public int Hour;
+    public int Minute;
+
+    public int Second;
+
+    public Schedule(int day, int hour, int minute, int second)
+    {
+        this.Day = day;
+        this.Hour = hour % 24;
+        this.Minute = minute % 60;
+        this.Second = second % 60;
+    }
+
+    public string getSchedule()
+    {
+        return this.Day + " " + this.Hour + ":" + this.Minute + ":" + this.Second;
+    }
+
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Schedule schedule = new Schedule(10, 250, 610, 610);
+        Console.WriteLine(schedule.getSchedule());
+
+        Schedule schedule2 = schedule;
+        Console.WriteLine(schedule2.getSchedule());
+    }
+}
