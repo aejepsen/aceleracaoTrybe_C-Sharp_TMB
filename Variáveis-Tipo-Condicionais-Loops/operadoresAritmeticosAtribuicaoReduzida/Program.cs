204bc128003afc0223f83ab92f927551bd58f3b8
allan <aejepsen@yahoo.com.br>
2023-02-17 18:50:36 -0300

    ok

---
+++ Variáveis-Tipo-Condicionais-Loops/operadoresAritmeticosAtribuicaoReduzida/Program.cs
@@ -0,0 +1,34 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+      // ++ incrementa +1
+      // -- decrementa -1
+      // += incrementa o valor da variável
+      // -= decrementa o valor da variável
+      // *= multiplica o valor da variável
+      // /= divide o valor da variável
+      // %= pega o resto da divisão do valor da variável
+      int a = 10;
+      a += 5;
+      Console.WriteLine(a);
+      // 15
+      a = 10;
+      a -= 5;
+      Console.WriteLine(a);
+      // 5
+      a = 10;
+      a *= 5;
+      Console.WriteLine(a);
+      // 50
+      a = 10;
+      a /= 5;
+      Console.WriteLine(a);
+      // 2
+      a = 10;
+      a %= 5;
+      Console.WriteLine(a);
+      // 0
+
+    }
+}
