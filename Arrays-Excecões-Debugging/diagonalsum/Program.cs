f7d0780ba4e580933e598408d48b49fd1e684e6e
allan <aejepsen@yahoo.com.br>
2023-03-08 18:40:51 -0300

    arraymultidimensional

---
+++ Arrays-Excecões-Debugging/diagonalsum/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+  static void Main(string[] args)
+  {
+    int[,] matrix = new int[3, 3] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+    int sum = getSumDiagonal(matrix);
+    Console.WriteLine(sum);
+  }
+
+  public static int getSumDiagonal(int[,] matrix)
+  {
+    int sum = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+      sum += matrix[i, i];
+    }
+    return sum;
+  }
+}
