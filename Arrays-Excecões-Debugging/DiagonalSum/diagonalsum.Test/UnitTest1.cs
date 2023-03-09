ba4e1f0e3dda7c7b81cc829977fb8ecf5daa85fe
allan <aejepsen@yahoo.com.br>
2023-03-08 22:34:39 -0300

    ok

--- Arrays-Excecões-Debugging/DiagonalSum/diagonalsum.Test/UnitTest1.cs
+++ Arrays-Excecões-Debugging/DiagonalSum/diagonalsum.Test/UnitTest1.cs
@@ -6,14 +6,31 @@ namespace diagonalsum.Test;
 
 public class UnitTest1
 {
-[Theory]
-[InlineData(new int[] { 1, 2, 3, 4 })]
-public void TestArrayInlineData(int[] intArrayEntry)
-{
-    for (int i=0; i < intArrayEntry.Length; i++) 
-    {
-        Console.WriteLine(intArrayEntry[i]);
 
-    }
+[Theory]
+[InlineData(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, 3, 3, 15)]
+[InlineData(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, 3, 3, 16)]
+
+
+public void fromArrayToTwoDimensionalArray(int[] intArrayEntry, int lines, int colums, int expected)
+{
+    int[,] multDimArray = Program.fromArrayToTwoDimensionalArrayOp1(intArrayEntry, lines, colums);
+
+    int sum = Program.getSumDiagonal(multDimArray);
+
+    sum.Should().Be(expected);
 }
+
+
+// [Theory]
+// [InlineData(new int[] { 1, 2, 3, 4 })]
+// public void TestArrayInlineData(int[] intArrayEntry)
+// {
+//     for (int i=0; i < intArrayEntry.Length; i++) 
+//     {
+//         Console.WriteLine(intArrayEntry[i]);
+
+//     }
+// }
+
 }
